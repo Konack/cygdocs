a85c890d1839464f97491b0004331f3d73a0a002
Konack <konack17@gmail.com>
2018-09-12 01:13:30 -0400

    Initial commit

---
+++ helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Written by Alec M
+//Written on 9/12/18
+//Written for gcc in c
+//Prints what is include inside the quotation marks following printf 
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
